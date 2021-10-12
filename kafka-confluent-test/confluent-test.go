3b19f2ec494a8634b3db7b733461e4c4556195bc
abel <xiaozanbiao@xiaoniuhy.com>
2021-10-12 15:34:54 +0800

    测试confluent-kafka；

---
+++ kafka-confluent-test/confluent-test.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+
+	// 当前只能支持Linux版本
+	// c, err := kafka.NewConsumer(&kafka.ConfigMap{
+	// 	"bootstrap.servers": "localhost",
+	// 	"group.id":          "myGroup",
+	// 	"auto.offset.reset": "earliest",
+	// })
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// c.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
+
+	// for {
+	// 	msg, err := c.ReadMessage(-1)
+	// 	if err == nil {
+	// 		fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+	// 	} else {
+	// 		// The client will automatically try to recover from all errors.
+	// 		fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+	// 	}
+	// }
+
+	// c.Close()
+}
